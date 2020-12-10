1f452e3e7d1ef381625a539e237222d10f48d89a
Loren Cipriano <loren25cipriano@gmail.com>
2020-12-10 14:31:28 -0800

    commit tree 6efb15cb66460ef7d23a0ccbec2ce385b2823f0b

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,33 @@
 require_relative "./vehicle.rb"
+
+
+class Car < Vehicle
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end 
+
+
+# car
+#   #new
+#     initializes with two arguments, wheel size and number
+#   #wheel_size
+#     has a wheel_size
+#   #wheel_number
+#     has a wheel_number
+#   #go
+#     returns 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+#   #fill_up_tank
+#     returns 'filling up!'
+
+# Vehicle
+#   #new
+#     initializes with two arguments, wheel size and number
+#   #wheel_size
+#     has a wheel_size
+#   #wheel_number
+#     has a wheel_number
+#   #go
+#     returns 'vrrrrrrrooom!'
+#   #fill_up_tank
+#     returns 'filling up!'
